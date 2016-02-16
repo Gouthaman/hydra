030388b5942193b5b027d838f4d4083d52276242
Gouthaman <segouthaman@gmail.com>
2016-02-16 17:22:28 +0530

    category controller added

---
+++ src/hydra/api/controllers/CategoryController.js
@@ -0,0 +1,14 @@
+/**
+ * CategoryController
+ *
+ * @description :: Server-side logic for managing categories
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	addCategory:function(req,res){
+		var data=req.allParams();
+		
+	}
+};
+
