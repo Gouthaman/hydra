71ca0983f623c32b51b97992b8552de23ed7a0ca
Pasupathi Ganesan <Pasupathi.Ganesan@happiestminds.com>
2016-02-17 17:11:44 +0530

    add and fetch category created

--- src/hydra/api/controllers/CategoryController.js
+++ src/hydra/api/controllers/CategoryController.js
@@ -6,9 +6,48 @@
  */
 
 module.exports = {
-	addCategory:function(req,res){
-		var data=req.allParams();
-		
-	}
+
+    defaultaction: function (req, res) {
+        return res.view("homepage", { result: undefined, status: "", message: "" });
+    },
+    addCategory: function (req, res) {
+        var data = req.allParams();
+
+        console.log(data.name);
+        if (data.name && data.name.length == 0)
+            return res.view("homepage", { result: undefined, status: "Failed", message: "Category Name empty" });
+
+        var selectcategory = Category.find();
+        selectcategory.where({ 'name': data.name });
+        selectcategory.exec(function (err, results) {
+            console.log(results);
+            if (results.length > 0) {
+                return res.view("homepage", { result: undefined, status: "Failed", message: "Category already exist" });
+            }
+            else {
+                Category.create({
+                    'name': data.name
+                }).exec(function (err, results) {
+                    console.log(results);
+                    if (err) {
+                        return res.view("homepage", { result: undefined, status: "Failed", message: "Category creation failed" });
+                    }
+                    return res.view("homepage", { result: undefined, status: "Success", message: "Category successfully created." })
+                });
+            }
+        });
+    },
+    getCategories: function (req, res) {
+
+        var getCategory = Category.find();
+        getCategory.exec(function (err, results) {
+            if (results.length > 0)
+                return res.view("homepage", { result: results, status: "", message: "" });
+            else
+                return res.view("homepage", { result: undefined, status: "", message: "No details found" });
+        });
+
+
+    }
 };
 
