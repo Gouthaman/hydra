5125f536850a2826f8437a8379b758231b5dbefc
Gouthaman <segouthaman@gmail.com>
2016-02-26 15:45:15 +0530

    Component create and retrive finished

---
+++ src/hydra/api/models/Component.js
@@ -0,0 +1,45 @@
+/**
+ * Component.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+  	id:{
+  		type: 'INTEGER',
+        primaryKey: true,
+        autoIncrement: true
+  	},
+  	name:{
+  		type: 'string',
+  		unique: true,
+  		required:true,
+  		size: 100
+  	},
+  	description:{
+  		type:'string',
+  		size:1024
+  	},
+  	owner:{
+  		type:'string',
+  		required: true
+  	},
+  	image:{
+  		type:'string'
+  	},
+  	referenceLink:{
+  		type:'string'
+  	},
+  	componentType:{
+  		model:'Category',
+  		required:true
+  	},
+  	versions:{
+  		collection:'version',
+  		via: 'component'
+  	}
+  }
+};
+
